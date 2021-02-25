aa5ddec12bae81e100e7ab718fa4c96e8b56d083
Kiki <kiki.ikik@gmail.com>
2021-02-25 15:00:28 +0100

    -a

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-'use strict';
+'use strict'
 
-const express = require('express');
-const { Server } = require('ws');
+const express = require('express')
+const { Server } = require('ws')
 
-const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const PORT = process.env.PORT || 3000
+const INDEX = '/index.html'
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-const wss = new Server({ server });
+const wss = new Server({ server })
 
+wss.on('connection', ws => {
+  console.log('Client connected')
 
-wss.on('connection', (ws) => {
-  console.log('Client connected');
-  ws.on('close', () => console.log('Client disconnected'));
-});
+  ws.on('message', data => {
+    console.log(data)
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === ws.OPEN) {
+        client.send(data)
+      }
+    })
+  })
 
-let test=["arr1","arr2"]
+  ws.on('close', () => console.log('Client disconnected'))
+})
 
+let test = ['arr1', 'arr2']
 
 setInterval(() => {
-  wss.clients.forEach((client) => {
+  wss.clients.forEach(client => {
     //client.send(new Date().toTimeString());
-    client.send(JSON.stringify(test));
-    
-  });
-}, 1000);
+    //client.send(JSON.stringify(test));
+  })
+}, 1000)
