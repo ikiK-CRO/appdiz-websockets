531c7594dc2b9dc203a6e832b2ea29a5d8004eee
Kiki <kiki.ikik@gmail.com>
2021-02-25 03:41:52 +0100

    array test

--- server.js
+++ server.js
@@ -12,13 +12,19 @@ const server = express()
 
 const wss = new Server({ server });
 
+
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('close', () => console.log('Client disconnected'));
 });
 
+let test=["arr1","arr2"]
+
+
 setInterval(() => {
   wss.clients.forEach((client) => {
-    client.send(new Date().toTimeString());
+    //client.send(new Date().toTimeString());
+    client.send(JSON.stringify(test));
+    
   });
 }, 1000);
